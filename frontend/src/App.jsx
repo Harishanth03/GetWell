84686e25e91774c9d8aedf8739c1cbd9e19c2fc6
Harishanth03 <kandeepaharishanth@gmail.com>
2024-12-20 21:24:19 +0530

    set Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
+import Doctor from './Pages/Doctor'
+import Login from './Pages/Login'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Myprofile from './Pages/Myprofile'
+import MyAppointment from './Pages/MyAppointment'
 
 const App = () => {
 
   return (
 
-    <div className='mx-4 sm:max-[10%]'>
+    <div className='mx-4 sm:mx-[10%]'>
 
       <Routes>
 
         <Route path='/' element={<Home/>}/>
+
+        <Route path='/doctor' element={<Doctor/>}/>
+
+        <Route path='/doctor/:speciality' element={<Doctor/>}/>
+
+        <Route path='/login' element={<Login/>}/>
+
+        <Route path='/about' element={<About/>}/>
+
+        <Route path='/contact' element={<Contact/>}/>
+
+        <Route path='/my-profile' element={<Myprofile/>}/>
+
+        <Route path='/my-appointments' element={<MyAppointment/>}/>
+
       </Routes>
 
     </div>
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-        
+    <div>This our Home Page!
     </div>
   )
 }
