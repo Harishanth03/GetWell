9de20c8db18a5c15e2ff34c7f530b65443280b92
Harishanth03 <kandeepaharishanth@gmail.com>
2024-12-20 21:06:56 +0530

    install packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 
 const App = () => {
 
   return (
 
-    <div >
+    <div className='mx-4 sm:max-[10%]'>
 
-      
+      <Routes>
+
+        <Route path='/' element={<Home/>}/>
+      </Routes>
 
     </div>
 
