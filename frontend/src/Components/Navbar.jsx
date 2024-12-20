e377a148dbdeb65b172696a62798b9a3fd133e1d
Harishanth03 <kandeepaharishanth@gmail.com>
2024-12-20 22:48:40 +0530

    install react router dom

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -5,48 +5,41 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
 
-    <div className='flex items-center justify-between text-sm py-4 mb-4 border-b border-b-gray-400'>
+    <div className='flex items-center justify-between text-sm py-4 mb-5 '>
 
-        <img src={assets.logo} alt="" />
+        <img className='w-[150]'  src={assets.logo} alt="" />
 
-        <ul>
-            <NavLink>
+        <ul className='hidden md:flex items-center gap-5 text-[#49577e] text-base'>
 
-                <li>Home</li>
+            <NavLink to={'/'}>
 
-                <hr />
+                <li className="py-1">Home</li>
+
+                <hr className='border-none outline-none h-0.5 bg-primary w-full m-auto hidden'/>
+                
+            </NavLink>
+
+            <NavLink to={'/doctors'}>
+
+                <li className="py-1">All Doctors</li>
+
+               <hr  className='border-none outline-none h-0.5 bg-primary w-full m-auto hidden'/>
 
             </NavLink>
 
-            <NavLink>
+            <NavLink to={'/about'}>
 
-                <li>All Doctors</li>
+                <li className="py-1">About</li>
 
-                <hr />
-
-            </NavLink>
-
-            <NavLink>
-
-                <li>About</li>
-
-                <hr />
+               <hr className='border-none outline-none h-0.5 bg-primary w-full m-auto hidden' />
 
             </NavLink>
 
-            <NavLink>
+            <NavLink to={'/contact'}>
 
-                <li>Contact</li>
+                <li className="py-1">Contact</li>
 
-                <hr />
-
-            </NavLink>
-
-            <NavLink>
-
-                <li>Home</li>
-
-                <hr />
+               <hr  className='border-none outline-none h-0.5 bg-primary w-full m-auto hidden'/>
 
             </NavLink>
 
