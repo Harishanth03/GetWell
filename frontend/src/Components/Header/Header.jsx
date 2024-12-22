2a514a44094538b17650139076ac211eedada1d8
Harishanth03 <kandeepaharishanth@gmail.com>
2024-12-22 20:52:40 +0530

    refactor Header component layout for better responsiveness and styling

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -6,13 +6,13 @@ const Header = () => {
 
   return (
    
-    <div className='flex '>
+    <div className='flex flex-col md:flex-row flex-wrap bg-primary px-6 md:px-10 lg:px-20 rounded-lg '>
 
         {/* ------------------------------------------------------- LEFT SIDE ------------------------------------------------------ */}
 
-        <div>
+        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
 
-            <p>
+            <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'> 
 
                 Book Appintment <br /> with trusted Doctors
 
