7ba8a1ea662186cc74db287a5e54c753167ee544
Harishanth03 <kandeepaharishanth@gmail.com>
2024-12-22 21:06:55 +0530

    refactor Header component for improved layout and responsiveness with flexbox styling

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -18,23 +18,23 @@ const Header = () => {
 
             </p>
 
-            <div>
+            <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
 
-                <img src={assets.group_profiles} alt="" />
+                <img className='w-28' src={assets.group_profiles} alt="" />
 
-                <p>Simply browse through our extensive list of trusted doctors, <br /> schedule your appointment hassle-free.</p>
+                <p>Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block'/> schedule your appointment hassle-free.</p>
 
             </div>
 
-            <a href="">Book Appointment <img src={assets.arrow_icon} alt="" /></a>
+            <a href="#specality" className='flex items-center gap-2 bg-white px-8 py-3 rounded-md text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300' href="">Book Appointment <img className='w-3' src={assets.arrow_icon} alt="" /></a>
 
         </div>
 
         {/* ------------------------------------------------------- RIGHT SIDE ------------------------------------------------------ */}
 
-        <div>
+        <div className='md:w-1/2 relative'>
 
-            <img src={assets.header_img} alt="" />
+            <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={assets.header_img} alt="" />
 
         </div>
 
