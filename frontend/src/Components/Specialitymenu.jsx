cf91fa0c6512bc76a3659038cdfb1ba71e6e3a80
Harishanth03 <kandeepanharishanth@gmail.com>
2024-12-24 23:35:58 +0530

    create TopDoctorsComponent

--- frontend/src/Components/Specialitymenu.jsx
+++ frontend/src/Components/Specialitymenu.jsx
@@ -3,7 +3,9 @@ import { specialityData } from '../assets/assets'
 import { Link } from 'react-router-dom'
 
 const Specialitymenu = () => {
+
   return (
+
     <div className='flex flex-col items-center gap-4 py-16' id='specality'>
 
         <h1 className='text-3xl font-medium'>Find by Speciality</h1>
@@ -23,9 +25,13 @@ const Specialitymenu = () => {
                 </Link>
 
             ))}
+
         </div>
+
     </div>
+
   )
+  
 }
 
 export default Specialitymenu
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import Header from '../Components/Header/Header'
 import Specialitymenu from '../Components/Specialitymenu'
+import TopDoctors from '../Components/TopDoctors'
 
 
 const Home = () => {
+
   return (
+
     <div>
+
       <Header />
+
       <Specialitymenu/>
+
+      <TopDoctors/>
+
     </div>
+
   )
+  
 }
 
 export default Home
