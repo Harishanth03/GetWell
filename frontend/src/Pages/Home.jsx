b8d087d221f2be70d7d01f89e4c4104d40b7e764
Harishanth03 <kandeepanharishanth@gmail.com>
2024-12-24 23:30:17 +0530

    create CSS for Specialitymenu

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../Components/Header/Header'
+import Specialitymenu from '../Components/Specialitymenu'
 
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Specialitymenu/>
     </div>
   )
 }
