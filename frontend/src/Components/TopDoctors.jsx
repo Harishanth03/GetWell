c089348b632a2aab323a6361bf3d7e0ee969032b
Harishanth03 <kandeepanharishanth@gmail.com>
2024-12-25 13:52:52 +0530

    style: update TopDoctors component to display a list of doctors with images and availability status

--- frontend/src/Components/TopDoctors.jsx
+++ frontend/src/Components/TopDoctors.jsx
@@ -6,8 +6,39 @@ const TopDoctors = () => {
   return (
 
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:my-0'>
+
       <h1 >Top Doctors to Book</h1>
+
       <p>Simply browse through our extensive list of doctors</p>
+
+      <div>
+
+        {doctors.slice(0,10).map((doctors , index) => (
+
+          <div key={index}>
+
+            <img src={doctors.image} alt="" />
+
+            <div>
+
+              <div>
+
+                <p></p> <p>Available</p>
+
+              </div>
+
+              <p>{doctors.name}</p>
+
+              <p>{doctors.speciality}</p>
+
+            </div>
+
+          </div>
+
+        ))}
+
+      </div>
+
     </div>
 
   )
