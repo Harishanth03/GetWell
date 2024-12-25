a871958079027bc15c5f094d7d17a69a71ccef8f
Harishanth03 <kandeepanharishanth@gmail.com>
2024-12-25 12:00:36 +0530

    implement TopDoctors component with doctor listings and a "More" button

--- frontend/src/Components/TopDoctors.jsx
+++ frontend/src/Components/TopDoctors.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import { doctors } from '../assets/assets'
 
 const TopDoctors = () => {
+
   return (
-    <div></div>
+
+    <div>
+
+      <h1>Top Doctors to Book</h1>
+
+      <p>Simply browse through our extensive list of trusted doctors</p>
+
+      <div>
+
+        {doctors.slice(0 , 10).map((doctors , index) => (
+
+          <div>
+
+            <img src={doctors.image} alt="" />
+
+            <div>
+
+              <div>
+
+                <p></p> <p>Available</p>
+
+              </div>
+
+              <p>{doctors.name}</p>
+
+              <p>{doctors.speciality}</p>
+
+            </div>
+
+          </div>
+
+        ))}
+
+      </div>
+
+      <button>More</button>
+
+    </div>
+
   )
+
 }
 
 export default TopDoctors
