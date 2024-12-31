d0870f37c242728106861f660df9f591d7a0f432
Harishanth03 <kandeepanharishanth@gmail.com>
2024-12-31 17:35:54 +0530

    style: update Footer component layout with responsive classes

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
 
   return (
 
-    <div>
+    <div className=''>
 
-        <div>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
 
             {/* ================================================ LEFT =================================================== */}
 
