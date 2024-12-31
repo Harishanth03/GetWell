3575afdd8cbf846d507ceb74d712d19a65aeb12e
Harishanth03 <kandeepanharishanth@gmail.com>
2024-12-31 20:27:21 +0530

    fix: implement filtering of doctors by speciality and enhance navigation in Doctor component

--- frontend/src/Pages/Doctor.jsx
+++ frontend/src/Pages/Doctor.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import { AppContext } from '../Context/AppContext';
 
 const Doctor = () => {
@@ -8,10 +8,81 @@ const Doctor = () => {
 
   const {doctors} = useContext(AppContext);
 
+  const navigate = useNavigate();
+
+  const [filterDoctors , setFilterDoctors] = useState([]);
+
+  const applyFilter = () => {
+
+    if(speciality)
+    {
+      setFilterDoctors(doctors.filter(doc => doc.speciality === speciality));
+    }
+    else
+    {
+      setFilterDoctors(doctors);
+    }
+
+  }
+
+  useEffect(() => {
+    
+    applyFilter();
+
+  } , [doctors , speciality]);
+
   return (
 
     <div>
         
+      <p>Browse using doctors speciality.</p>
+
+      <div>
+
+        <div>
+
+          <p>General physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p>
+
+        </div>
+        {/* ===================================================== RIGHT ================================================ */}
+        <div className=''>
+
+          {
+           
+            filterDoctors.map((item , index) => (
+
+              <div onClick={() => navigate(`/appointment/:${item._id}`)} key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer'>
+
+                <img className='bg-blue-50' src={item.image} alt="" />
+
+                <div className='p-4 text-center sm:text-start'>
+
+                  <div className='flex items-center justify-center sm:justify-normal gap-2 text-sm text-center text-green-500'>
+
+                    <p className='w-2 h-2 bg-green-500 rounded-full'></p> <p>Available</p>
+
+                  </div>
+
+                  <p className='text-gray-900 text-base font-normal'>{item.name}</p>
+
+                  <p className='text-gray-600 text-sm'>{item.speciality}</p>
+
+                </div>
+
+              </div>
+
+            ))
+
+          }
+          
+        </div>
+
+      </div>
 
     </div>
 
