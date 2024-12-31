692bd0ef63cac28b7fb295dae252fe59116c9b72
Harishanth03 <kandeepanharishanth@gmail.com>
2024-12-31 22:27:24 +0530

    style: enhance layout and presentation of doctor details in Appointment component

--- frontend/src/Pages/Appointment.jsx
+++ frontend/src/Pages/Appointment.jsx
@@ -44,23 +44,25 @@ const Appointment = () => {
 
           <p className='flex items-center gap-2 text-xl font-medium text-gray-900'>{docInfo.name} <img className='w-5' src={assets.verified_icon} alt="" /></p>
 
-          <div>
+          <div className='flex items-center gap-2 mt-3 text-sm text-gray-500'>
 
             <p>{docInfo.degree} - {docInfo.speciality}</p>
 
-            <button>{docInfo.experience}</button>
+            <button className='py-0.5 border px-2 text-xs rounded-full'>{docInfo.experience}</button>
 
           </div>
 
           {/* ================================================ Comment ================================================= */}
 
-          <div>
+          <div className=''>
 
-            <p>About <img src={assets.info_icon} alt="" /></p>
+            <p className='flex items-center gap-2 text-sm font-medium text-gray-900 mt-3'>About <img src={assets.info_icon} alt="" /></p>
 
-            <p>{docInfo.about}</p>
+            <p className='text-sm text-gray-500 max-w-[700px] mt-3'>{docInfo.about}</p>
 
           </div>
+
+          <p>Appointment Fees: <span>{docInfo.fees}</span></p>
 
 
         </div>
