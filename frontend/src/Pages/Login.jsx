165c2a5ce818eafd855d8224bd6ccc31cff71586
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-02 15:20:31 +0530

    feat: update Login component to improve state management and form structure

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const Login = () => {
 
-  const [state , setState] = useState('SignUp'); // first create the state variable to switch between login and signup
+  const [state , setState] = useState('Sign Up'); // first create the state variable to switch between login and signup
 
   const [email , setEmail] = useState(''); // create the email state variable
 
@@ -10,18 +10,45 @@ const Login = () => {
 
   // ================================================== ONSUBMIT FUNCTION ====================================================
 
-  const onSubmit = async(event) => {
+  const onSubmit = async(e) => {
 
     event.prevenDefault(); // prevent the default action of the form
-    
+
   }
 
 
   return (
 
-    <form action="">
+    <form action="" className='min-h-[80vh] flex items-center'>
 
+      <div>
 
+        <p>{state === 'Sign Up' ? 'Create Account' : 'LogIn'}</p>
+
+        <p>Please {state === 'Sign Up' ? 'Sign Up' : 'LogIn'} To Book Appointment</p>
+
+        <div>
+
+          <label htmlFor="fullName">Full Name</label>
+          <input id='fullName' value={name} onChange={() => setname(e.target.name)} type="text" required/>
+
+        </div>
+
+        <div>
+
+          <label htmlFor="Email">Email</label>
+          <input id='Email' value={email} onChange={() => setname(e.target.name)} type="email" required/>
+          
+        </div>
+
+        <div>
+
+          <label htmlFor="Password">Full Name</label>
+          <input id='Password' value={name} onChange={() => setname(e.target.name)} type="password" required/>
+          
+        </div>
+
+      </div>
 
     </form>
   )
