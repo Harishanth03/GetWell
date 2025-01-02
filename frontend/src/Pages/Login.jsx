984a1d8e6b63c3b7d48b0865d7c603e9848d8627
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-02 15:08:40 +0530

    feat: implement state management for Login component with email and name fields

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state , setState] = useState('SignUp'); // first create the state variable to switch between login and signup
+
+  const [email , setEmail] = useState(''); // create the email state variable
+
+  const [name , setname] = useState(''); // create the password state variable
+
+  // ================================================== ONSUBMIT FUNCTION ====================================================
+
+  const onSubmit = async(event) => {
+
+    event.prevenDefault(); // prevent the default action of the form
+    
+  }
+
+
   return (
-    <div>
-        
-    </div>
+
+    <form action="">
+
+
+
+    </form>
   )
 }
 
