b1fa405aa1f43618566390299ec43799d23ffe09
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-03 22:21:04 +0530

    feat: include image file handling in addDoctor API for improved doctor addition process

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -8,8 +8,14 @@ const addDoctor = async(req , res) => {
     try 
     {
 
-        const {name , email , password , image ,  speciality , degree , experience, about , available , fees , address , date , slots_booked} = req.body;
+        const {name , email , password , speciality , degree , experience, about , available , fees , address , date , slots_booked} = req.body;
+
+        const imageFile = req.file;
+
+        console.log({name , email , password ,speciality , degree , experience, about , available , fees , address , date , slots_booked}, imageFile)
+
         
+
     } 
     catch (error) 
     {
