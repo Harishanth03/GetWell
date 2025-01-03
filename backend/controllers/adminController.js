4f9e5409f8a723e33142a4c50863d0de9ad685ee
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-03 21:05:12 +0530

    feat: implement addDoctor API in adminController for managing doctor additions

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,19 @@
+
+
+
+//==================================================== API for adding Doctor ==============================================
+
+const addDoctor = async(req , res) => {
+
+    try 
+    {
+        const {name , email , password , image ,  speciality , degree , experience, about , available , fees , address , date , slots_booked} = req.body;
+
+        
+    } 
+    catch (error) 
+    {
+        
+    }
+
+}
