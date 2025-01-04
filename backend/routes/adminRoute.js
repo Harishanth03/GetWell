81fd6483d32f22e6727daf65380557bfec373593
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-04 21:41:38 +0530

    feat: add authentication middleware for admin routes

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -3,10 +3,11 @@ import express from 'express';
 import { addDoctor, loginAdmin } from '../controllers/adminController.js';
 
 import upload from '../middlewares/multer.js';
+import AuthAdmin from '../middlewares/AuthAdmin.js';
 
 const adminRouter = express.Router();
 
-adminRouter.post('/add-doctor', upload.single('image') , addDoctor);
+adminRouter.post('/add-doctor',AuthAdmin, upload.single('image') , addDoctor);
 
 adminRouter.post('/login' , loginAdmin);
 
