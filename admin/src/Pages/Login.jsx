2851920ae00a25849ef3574b5499c84e9012c643
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-06 14:39:17 +0530

    feat: improve Login component styling and layout for better user experience

--- admin/src/Pages/Login.jsx
+++ admin/src/Pages/Login.jsx
@@ -6,29 +6,29 @@ const Login = () => {
 
   return (
 
-    <form className='min-h-[80vh] flex items-center'>
+    <form className='min-h-[80vh] flex items-center justify-center'>
 
       <div className='flex flex-col gap-3 m-auto md:items-start p-8 m-w-[320px] sm:min-w-96 border rounded-xl text-[#5E5E5E]text-sm shadow-lg'>
 
-        <p className='text-2xl text-center m-auto font-semibold'><span>{state}</span> Login</p>
+        <p className='text-2xl text-center m-auto font-semibold'><span className='text-primary'>{state}</span> Login</p>
 
-        <div>
+        <div className='w-full'>
 
           <p>Email</p>
 
-          <input type="email"  required/>
+          <input className='border border-[#DADADA] rounded w-full p-1 mt-1 outline-primary' type="email"  required/>
 
         </div>
 
-        <div>
+        <div className='w-full'>
 
           <p>Password</p>
           
-          <input type="email"  required/>
+          <input className='border border-[#DADADA] rounded w-full p-1 mt-1 outline-primary' type="email"  required/>
 
         </div>
 
-        <button>Login</button>
+        <button className='bg-primary text-white w-full py-2 rounded-md mt-1 text-md'>Login</button>
 
       </div>
       
