6ba5a8e2894e13e0b43f3eb41bde050e476725b7
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-08 08:06:28 +0530

    feat: enhance Navbar styling for user role display and logo presentation

--- admin/src/Components/Navbar.jsx
+++ admin/src/Components/Navbar.jsx
@@ -10,11 +10,11 @@ const Navbar = () => {
 
     <div className='flex justify-between px-4 sm:px-10 py-3 border-b bg-white'>
 
-        <div>
+        <div className='flex items-center gap-2 text-sm'>
 
-            <img src={assets.logo}/>
+            <img className='w-36 sm:w-40 cursor-pointer' src={assets.logo}/>
 
-            <p>{atoken ? "Admin" : "Doctor"}</p>
+            <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{atoken ? "Admin" : "Doctor"}</p>
 
         </div>
 
