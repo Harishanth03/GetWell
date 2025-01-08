21a53ee499e9f50c9c9c11523361350f68f1c05b
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-08 09:31:53 +0530

    feat: add file upload and input fields to AddDoctor page

--- admin/src/Pages/Admin/AddDoctor.jsx
+++ admin/src/Pages/Admin/AddDoctor.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {assets} from '../../assets/assets.js'
 
 const AddDoctor = () => {
   return (
@@ -7,7 +8,40 @@ const AddDoctor = () => {
 
       <p>Add Doctors</p>
 
-      
+      <div>
+
+        <div>
+
+          <label htmlFor="upload-area">
+
+            <img src={assets.upload_area} />
+
+          </label>
+
+          <input type="file" hidden id="upload-area" />
+
+          <p>Upload doctor<br/>picture</p>
+
+        </div>
+
+        <div>
+
+          <div>
+
+            <div>
+
+              <p>Your Name</p>
+
+              <input type="text" id="" placeholder='Your Name' required/>
+
+            </div>
+
+          </div>
+          
+        </div>
+
+      </div>
+
     </form>
   )
 }
