0c76f59c38e732d5f9717ad9ee35267023938dca
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-08 11:57:11 +0530

    feat: expand AddDoctor form with additional fields for email, password, experience, fee, education, address, and about section

--- admin/src/Pages/Admin/AddDoctor.jsx
+++ admin/src/Pages/Admin/AddDoctor.jsx
@@ -36,9 +36,103 @@ const AddDoctor = () => {
 
             </div>
 
+            <div>
+
+              <p>Doctor Email</p>
+
+              <input type="email" id="" placeholder='Doctor Email' required/>
+
+            </div>
+
+            <div>
+
+              <p>Doctor Password</p>
+
+              <input type="password" id="" placeholder='Doctor Password' required/>
+
+            </div>
+
+            <div>
+
+              <p>Doctor Experience</p>
+
+              <select id="experience">
+
+                <option value="1-Year">1-Year</option>
+                <option value="2-Years">2-Years</option>
+                <option value="3-Years">3-Years</option>
+                <option value="4-Years">4-Years</option>
+                <option value="5-Years">5-Years</option>
+                <option value="6-Years">6-Years</option>
+                <option value="7-Years">7-Years</option>
+                <option value="8-Years">8-Years</option>
+                <option value="9-Years">9-Years</option>
+                <option value="10-Years">10-Years</option>
+
+              </select>
+
+            </div>
+
+            <div>
+
+              <p>Doctor Fee</p>
+
+              <input type="number" id="" placeholder='Doctor Fee' required/>
+
+            </div>
+
           </div>
-          
+
+          <div>
+
+            <div>
+
+              <p htmlFor="speciality" >Speciality</p>
+
+              <select  id="speciality">
+
+                <option value="General physician">General physician</option>
+                <option value="Gynecologist">Gynecologist</option>
+                <option value="Dermatologist">Dermatologist</option>
+                <option value="Pediatricians">Pediatricians</option>
+                <option value="Neurologist">Neurologist</option>
+                <option value="Gastroenterologist">Gastroenterologist</option>
+
+              </select>
+
+            </div>
+
+            <div>
+
+              <p>Education</p>
+
+              <input type="text" id="" placeholder='Education' required/>
+
+            </div>
+
+            <div>
+
+              <p>Address</p>
+
+              <input type="text" id="" placeholder='Address ' required/>
+
+              <input type="text" placeholder='Address 2'/>
+
+            </div>
+
+          </div>
+
         </div>
+
+        <div>
+
+          <p>About Doctor</p>
+
+          <textarea  id="" placeholder='About Doctor' required/>
+
+        </div>
+
+        <button>Add Doctor</button>
 
       </div>
 
