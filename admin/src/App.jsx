1f4e2f3fc798695d4d5273890f241c30374723c4
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-08 08:27:29 +0530

    feat: add routing for Admin pages including AddDoctor, AllAppointments, Dashboard, and DoctorsList

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,11 @@ import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from './Context/AdminContext';
 import Navbar from './Components/Navbar';
 import Sidebar from './Components/Sidebar';
+import {Route , Routes} from 'react-router-dom'
+import AddDoctor from './Pages/Admin/AddDoctor';
+import AllAppointments from './Pages/Admin/AllAppointments';
+import Dashboard from './Pages/Admin/Dashboard';
+import DocotorsList from './Pages/Admin/DocotorsList';
 
 const App = () => {
 
@@ -20,6 +25,20 @@ const App = () => {
       <div className='flex items-start'>
 
         <Sidebar/>
+
+        <Routes>
+
+          <Route path='/' element={<></>}/>
+
+          <Route path='/add-doctor' element={<AddDoctor/>}/>
+
+          <Route path='/all-appoinment' element={<AllAppointments/>}/>
+
+          <Route path='/dashboard' element={<Dashboard/>}/>
+
+          <Route path='/doctors-list' element={<DocotorsList/>}/>
+
+        </Routes>
 
       </div>
       
