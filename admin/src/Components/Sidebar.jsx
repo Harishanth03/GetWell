8bbeeb6ea3f2052cc70b9eed724c3c373884664d
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-08 08:46:44 +0530

    style: enhance Sidebar component with improved styling and active link indication

--- admin/src/Components/Sidebar.jsx
+++ admin/src/Components/Sidebar.jsx
@@ -9,35 +9,35 @@ const Sidebar = () => {
 
   return (
 
-    <div >
+    <div className='min-h-screen bg-white border-r'>
       
       {
 
         atoken && 
-        <ul>
+        <ul className='text-[#515151] mt-5'>
 
-          <NavLink to={'/dashboard'}>
+          <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:m-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-primary' : ''}`} to={'/dashboard'}>
 
             <img src={assets.home_icon} alt="" />
             <p>Dashboard</p>
             
           </NavLink> 
 
-          <NavLink to={'/all-appointment'}>
+          <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:m-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-primary' : ''}`} to={'/all-appointment'}>
 
             <img src={assets.appointment_icon} alt="" />
             <p>All Appointments</p>
             
           </NavLink> 
 
-          <NavLink to={'/add-doctor'}>
+          <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:m-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-primary' : ''}`} to={'/add-doctor'}>
 
             <img src={assets.add_icon} alt="" />
             <p>Add Doctors</p>
             
           </NavLink> 
 
-          <NavLink to={'/doctor-list'}>
+          <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:m-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-primary' : ''}`} to={'/doctor-list'}>
 
             <img src={assets.people_icon} alt="" />
             <p>Doctors List</p>
