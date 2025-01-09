4fd1c39b3e2451cc119936b36ed4bf5ee484d132
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-09 22:38:07 +0530

    feat: implement functionality to retrieve and display all doctors in the Admin context

--- admin/src/Pages/Admin/DocotorsList.jsx
+++ admin/src/Pages/Admin/DocotorsList.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { AdminContext } from '../../Context/AdminContext'
 
 const DocotorsList = () => {
+
+  const {doctors , atoken , getAllDoctors} = useContext(AdminContext);
+
+  useEffect(() => {
+
+    if(atoken)
+    {
+      getAllDoctors()
+    }
+
+
+  }, [atoken])
+
   return (
-    <div>DocotorsList</div>
+
+    <div>
+      
+      <h1></h1>
+      
+
+    </div>
   )
 }
 
