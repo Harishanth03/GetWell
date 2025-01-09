e01ceab63628a5d7905004733d83a77f920dfa58
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-09 22:49:49 +0530

    feat: enhance DoctorsList component with improved layout and doctor availability display

--- admin/src/Pages/Admin/DocotorsList.jsx
+++ admin/src/Pages/Admin/DocotorsList.jsx
@@ -17,9 +17,39 @@ const DocotorsList = () => {
 
   return (
 
-    <div>
+    <div className='m-5 max-h-[90vh] overflow-y-scroll'>
       
-      <h1></h1>
+      <h1 className='text-lg font-medium'>All Doctors</h1>
+
+      <div className='flex flex-wrap gap-4 gap-y-6'>
+
+        {
+
+          doctors.map((doctor , index) => (
+
+            <div className='border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer group' key={index}>
+
+              <img className='bg-indigo-50 group-hover:bg-primary transition-all duration-150' src={doctor.image} alt="" />
+
+              <div>
+
+                <p>{doctor.name}</p>
+                <p>{doctor.speciality}</p>
+
+              </div>
+
+              <div>
+
+                <input type="checkbox" checked={doctor.available}/>
+                <p>Available</p>
+
+              </div>
+
+            </div>
+          ))
+
+        }
+      </div>
       
 
     </div>
