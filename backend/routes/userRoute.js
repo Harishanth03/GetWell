7a6c417d74df9862d94b1a7d00e378d7b482a8e0
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-17 15:19:24 +0530

    feat: add login endpoint to userRouter for user authentication

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { registerUser } from '../controllers/userController.js';
+import { registerUser, userLogIn } from '../controllers/userController.js';
 const userRouter = express.Router();
 
 userRouter.post('/register' , registerUser);
+
+userRouter.post('/login' , userLogIn);
 
 export default userRouter;
