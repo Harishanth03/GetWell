310a70c905be127ca21f84f865eca5a3e2deff08
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-17 14:19:15 +0530

    feat: add error handling with toast notifications in AppContextProvider

--- frontend/src/Context/AppContext.jsx
+++ frontend/src/Context/AppContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 
 export const AppContext = createContext();
@@ -33,12 +34,17 @@ const AppContextProvider = props =>
                 setDoctor(data.doctors)
 
             }
+            else
+            {
+                toast.error(data.message)
+            }
 
         }
         catch(error)
         {
 
             console.log(error)
+            toast.error(error.message)
 
         }
     }
