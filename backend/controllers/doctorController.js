629170ea16f83b0155c0a7dcf9c5e4707a66e424
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-17 14:23:08 +0530

    feat: add doctorList function to fetch and return a list of doctors without sensitive information

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -25,4 +25,26 @@ const changeAvailablity = async(req , res) => {
 
 }
 
-export{changeAvailablity}
+//============================================== All doctors ===============================================
+
+const doctorList = async(req , res) => {
+
+    try 
+    {
+
+        const doctors = await doctorModel.find({}).select(['-password', '-email']) // remove password and email
+
+        res.json({success:true , doctors})
+        
+    } catch (error) 
+    {
+
+        console.log(error);
+
+        res.json({success: false , message: error.message});   
+        
+    }
+
+}
+
+export{changeAvailablity , doctorList}
