94c7ab3ae358fa5ba3b74f0c608d0230883084e6
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-20 22:50:23 +0530

    fix: correct input change handlers in Login component for proper state updates

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -98,7 +98,7 @@ const Login = () => {
           <div className='w-full'>
 
             <p htmlFor="fullName">Full Name</p>
-            <input className='w-full border sm:py-2 focus:border-primary focus:outline-none border-zinc-200 rounded-sm py-1 px-2 mt-1' id='fullName' value={name} onChange={() => setname(e.target.name)} type="text" required/>
+            <input  className='w-full border sm:py-2 focus:border-primary focus:outline-none border-zinc-200 rounded-sm py-1 px-2 mt-1' id='fullName' onChange={(e) => setname(e.target.value)} value={name} type="text" required/>
 
           </div> : null
 
@@ -107,18 +107,18 @@ const Login = () => {
         <div className='w-full'>
 
           <p htmlFor="Email">Email</p>
-          <input className='w-full border sm:py-2 focus:border-primary focus:outline-none border-zinc-200 rounded-sm py-1 px-2 mt-1'  id='Email' value={email} onChange={() => setEmail(e.target.name)} type="email" required/>
+          <input  className='w-full border sm:py-2 focus:border-primary focus:outline-none border-zinc-200 rounded-sm py-1 px-2 mt-1'  id='Email' value={email} onChange={(e) => setEmail(e.target.value)} type="email" required/>
           
         </div>
 
         <div className='w-full'>
 
           <p htmlFor="Password">Password</p>
-          <input className='w-full border sm:py-2 focus:border-primary focus:outline-none border-zinc-200 rounded-sm py-1 px-2 mt-1'   id='Password' value={name} onChange={() => setPassword(e.target.name)} type="password" required/>
+          <input  className='w-full border sm:py-2 focus:border-primary focus:outline-none border-zinc-200 rounded-sm py-1 px-2 mt-1'   id='Password' value={password} onChange={(e) => setPassword(e.target.value)} type="password" required/>
           
         </div>
 
-        <button type='submit' className='py-2 px-auto text-base bg-primary text-white rounded-sm'>{state === 'Sign Up' ? "Sign Up" : "Sign In"}</button>
+        <button type='submit ' className='py-2 px-auto text-base bg-primary text-white rounded-sm'>{state === 'Sign Up' ? "Sign Up" : "Sign In"}</button>
 
         {
           state === 'Sign Up' ? <p className='text-center'>Already have an account? <span className='text-primary cursor-pointer' onClick={() => setState('Log In')}>Log In</span></p> : <p className='text-center'>Don't have an account? <span className='text-primary cursor-pointer' onClick={() => setState('Sign Up')}>Sign Up</span></p>
