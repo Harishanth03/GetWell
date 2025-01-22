2266574a6a58c7f6b5bbf67cb2261b80a6c60e2c
Harishanth03 <kandeepanharishanth@gmail.com>
2025-01-22 10:22:54 +0530

    fix: update token handling in AuthUser middleware for consistency and improved validation

--- backend/middlewares/AuthUser.js
+++ backend/middlewares/AuthUser.js
@@ -7,23 +7,18 @@ const AuthUser = async(req , res , next) => {
     try 
     {
 
-        const {atoken} = req.headers; //get the token from the headers
+        const {token} = req.headers; //get the token from the headers
 
-        if(!atoken) 
+        if(!token) 
         {
 
             return res.json({success: false , message: "Access Denied"}); //if no token is provided then return this message
 
         }
 
-        const tokenDecode = jwt.verify(atoken , process.env.JWT_SECRET); //verify the token
+        const tokenDecode = jwt.verify(token , process.env.JWT_SECRET); //verify the token
 
-        if(tokenDecode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD)
-        {
-
-            return res.json({success: false , message: "Invalid Token"}); //if token is invalid then return this message
-
-        }
+        req.body.userId = tokenDecode.id
 
         next(); //if token is valid then call the next middleware
         
